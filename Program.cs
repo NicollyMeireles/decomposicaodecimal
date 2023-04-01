0e29258592297eb4990bdea1cd835df27a23f311
NicollyMeireles <nicollymeireles32@gmail.com>
2023-03-31 22:29:24 -0300

    decomposicaodecimal

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("DECOMPOSIÇÃO DECIMAL");
+
+Console.Write("Digite um número inteiro...: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+int restante = numero;
+int unidades = restante % 10;
+restante /= 10;
+int dezenas = restante % 10;
+restante /= 10;
+int centenas = restante;
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"\nO número {numero} possui:");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"{unidades,10} unidade(s)");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"{dezenas,10} dezenas(s)");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"{centenas,10} centena(s).");
+
